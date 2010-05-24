64fa2ac2185e7a4424f027da8b418540c7d5c2a3
Doc Yes <spam@featureblend.com>
2010-05-24 10:21:58 -0700

    Add clip parser and port and debug options. python main.py --help will list available args that can be passed at startup (ie., python main.py --port=2010)

--- web/app/main.py
+++ web/app/main.py
@@ -3,6 +3,10 @@ import tornado.ioloop
 import tornado.web
 from tornado.options import define, options
 
+# App options
+define("port", default=8888, help="web server port", type=int)
+define("debug", default=False, help="debug mode, will force re-compilation of templates and auto-reload of True", type=bool)
+
 # Controllers
 from controllers.issues import IssuesController
 
@@ -11,6 +15,7 @@ application = tornado.web.Application([
 ])
 
 if __name__ == "__main__":
+    tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(application)
-    http_server.listen(8888)
+    http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
